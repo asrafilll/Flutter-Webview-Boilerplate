8fe52b1401faa86a7c27060fbc9283f3c1db4704
asrafilll <asrafilr@gmail.com>
2023-03-19 19:31:26 +0700

    Update WebView version

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_boilerplate/webview.dart';
+import 'package:flutter_webview_boilerplate/webview_page.dart';
 
-void main() => runApp(MainApp());
-
-class MainApp extends StatefulWidget {
-  @override
-  _MainAppState createState() => _MainAppState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _MainAppState extends State<MainApp> {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Webview(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const WebviewPage(),
     );
   }
 }
